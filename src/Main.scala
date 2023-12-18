160628d704e20af32560456a0d91eee538dcd4f2
Joshua <siedeltoad@gmail.com>
2023-12-18 21:40:45 +0100

    Test de bon fonctionnement 3 !

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,16 @@
-object Main {
+/*object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
+}*/
+
+object Main extends App{
+
+  // TODO : Y a pas grand chose d'écrit.
+
 }
+
+
+
+
+
