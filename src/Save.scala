63614f02bee144269edd64bce2c80e40afadfefc
Discretos <siedeljosh@gmail.com>
2024-01-15 15:51:24 +0100

    Save update

--- src/Save.scala
+++ src/Save.scala
@@ -4,38 +4,29 @@ import java.io.{BufferedOutputStream, FileOutputStream, PrintStream}
 
 object Save {
 
+  // Path of the save file
   var file:String = "res/save.txt";
 
+  // Write highscore in the save file
   def WriterSave(): Unit = {
-
     val f = new PrintStream(new BufferedOutputStream(new FileOutputStream(file)))
-
     try {
       for(i:Int <-0 until bestScores.length)
         f.println(bestScores(i))
-
-      f.close();
+      f.close()
     }
-
   }
 
+  // Read the save file
   def ReadSave(): Unit = {
-
     try{
       val f = scala.io.Source.fromFile(file)
       val data: Array[String] = f.getLines().toArray
       f.close()
-
       for (i: Int <- 0 until data.length) {
-
         bestScores(i) = java.lang.Integer.parseInt(data(i))
-
         println(Integer.getInteger(data(i)))
-
       }
-
     }
-
   }
-
 }
